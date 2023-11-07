a36d0ab5e9fffe0d2d522d0ba5eb1a4a857ded70
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-11-07 16:39:32 +0200

    added timer script for the game and simple main menu scene

---
+++ Assets/Scriptit/Playername.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    public InputField nameInputField;
+    public static string playerName;
+
+    public void SavePlayerName()
+    {
+        playerName = nameInputField.text;
+        Debug.Log("Player Name: " + playerName);
+    }
+}
