e6ad28dc7df4da0f91d6e1a699c5742d71e0d627
Olli Saarenmaa <o.saarenmaa@gmail.com>
2023-12-04 03:46:42 +0200

    Liukuminen estetty

--- Assets/Punainen_auto/Punainen_auto.cs
+++ Assets/Punainen_auto/Punainen_auto.cs
@@ -13,12 +13,29 @@ public class Punainen_auto : MonoBehaviour
     }
     void Update()
     {
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.5f;
-        Ray ray = new Ray(rayOrigin, -transform.forward);
+        // Straight back rays
+        Vector3 rayOrigin1 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection1 = -transform.forward;
+
+        Vector3 rayOrigin2 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection2 = Quaternion.Euler(0, 45, 0) * -transform.forward; // 45 degrees to the right
+
+        Vector3 rayOrigin3 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection3 = Quaternion.Euler(0, -45, 0) * -transform.forward; // 45 degrees to the left
+
+        RaycastAndHandle(rayOrigin1, rayDirection1);
+        RaycastAndHandle(rayOrigin2, rayDirection2);
+        RaycastAndHandle(rayOrigin3, rayDirection3);
+    }
+
+    void RaycastAndHandle(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin, direction);
         RaycastHit hit;
+
         Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red);
 
-        if (Physics.Raycast(ray, out hit, raycastDistance) && script.carFront == false)
+        if (Physics.Raycast(ray, out hit, raycastDistance))
         {
             if (hit.collider.CompareTag("Player"))
             {
--- Assets/Vihreä_auto/VihreäAuto.cs
+++ Assets/Vihreä_auto/VihreäAuto.cs
@@ -9,9 +9,26 @@ public class VihreäAuto : MonoBehaviour
 
     void Update()
     {
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.5f;
-        Ray ray = new Ray(rayOrigin, -transform.forward);
+        // Straight back rays
+        Vector3 rayOrigin1 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection1 = -transform.forward;
+
+        Vector3 rayOrigin2 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection2 = Quaternion.Euler(0, 45, 0) * -transform.forward; // 45 degrees to the right
+
+        Vector3 rayOrigin3 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection3 = Quaternion.Euler(0, -45, 0) * -transform.forward; // 45 degrees to the left
+
+        RaycastAndHandle(rayOrigin1, rayDirection1);
+        RaycastAndHandle(rayOrigin2, rayDirection2);
+        RaycastAndHandle(rayOrigin3, rayDirection3);
+    }
+
+    void RaycastAndHandle(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin, direction);
         RaycastHit hit;
+
         Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red);
 
         if (Physics.Raycast(ray, out hit, raycastDistance))
@@ -22,5 +39,6 @@ public class VihreäAuto : MonoBehaviour
             }
         }
     }
+
 }
 
