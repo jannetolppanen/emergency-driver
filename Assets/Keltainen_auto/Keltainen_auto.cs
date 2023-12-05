a5c5da9ca8b11704c7eacb0f0db745a30f440de3
Olli Saarenmaa <o.saarenmaa@gmail.com>
2023-12-05 11:36:28 +0200

    Lisää korjailua

--- Assets/Keltainen_auto/Keltainen_auto.cs
+++ Assets/Keltainen_auto/Keltainen_auto.cs
@@ -6,7 +6,7 @@ public class Keltainen_auto : MonoBehaviour
 {
     public float raycastDistance;
     public GameObject brakeLight;
-    public testiajelu script;
+    private testiajelu script;
 
     void Start()
     {
@@ -14,12 +14,29 @@ public class Keltainen_auto : MonoBehaviour
     }
     void Update()
     {
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.5f;
-        Ray ray = new Ray(rayOrigin, -transform.forward);
+        // Straight back rays
+        Vector3 rayOrigin1 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection1 = -transform.forward;
+
+        Vector3 rayOrigin2 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection2 = Quaternion.Euler(0, 30, 0) * -transform.forward; // 30 degrees to the right
+
+        Vector3 rayOrigin3 = transform.position + Vector3.up * 0.5f;
+        Vector3 rayDirection3 = Quaternion.Euler(0, -30, 0) * -transform.forward; // 30 degrees to the left
+
+        RaycastAndHandle(rayOrigin1, rayDirection1);
+        RaycastAndHandle(rayOrigin2, rayDirection2);
+        RaycastAndHandle(rayOrigin3, rayDirection3);
+    }
+
+    void RaycastAndHandle(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin, direction);
         RaycastHit hit;
+
         Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red);
 
-        if (Physics.Raycast(ray, out hit, raycastDistance) && script.carFront == false)
+        if (Physics.Raycast(ray, out hit, raycastDistance))
         {
             if (hit.collider.CompareTag("Player"))
             {
